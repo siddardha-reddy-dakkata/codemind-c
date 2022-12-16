a1cbe135b0a67e481112974f459a65eae09b572d
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2022-12-16 20:26:04 +0530

    #include<stdio.h> int main(){     int i,a;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         if(i%2==1)         {             printf("%d ",i);             }
        }
        
    }

---
+++ Odd Natural_Numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){ 
+    int i,a;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++) 
+    { 
+        if(i%2==1)
+        {
+            printf("%d ",i);
+            } 
+        
+    }
+    
+}
