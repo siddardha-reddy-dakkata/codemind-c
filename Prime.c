62a9715a32b545d3980a3733b14372dca7b9520e
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2022-12-21 18:23:18 +0530

    #include<stdio.h> int main(){int a,i,count=0;scanf("%d",&a);for(i=1;i<=a;i++){if(a%i==0){count+=1;}}if(count==2){printf("Prime");}else{printf("Not Prime");}}

--- Prime.c
+++ Prime.c
@@ -1,15 +1,2 @@
 #include<stdio.h>
-int main(){
-    int i,a,count=0;
-    scanf("%d",&a); 
-    for(i=1;i<=a;i++)
-    {
-        if(a%i==0) 
-        count++; 
-        
-    } if (count==2)
-    {
-        printf("Prime");
-    }
-    else printf("Not Prime");
-}
+int main(){int a,i,count=0;scanf("%d",&a);for(i=1;i<=a;i++){if(a%i==0){count+=1;}}if(count==2){printf("Prime");}else{printf("Not Prime");}}
