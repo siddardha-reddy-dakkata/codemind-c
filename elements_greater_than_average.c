cd413d2955675b1e29ec76aea4d98be6049e71ed
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2022-12-27 12:04:18 +0530

    #include<stdio.h> int main() {     int n,c=0,s=0;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         s=s+a[i];     }     int avg=s/n;     for(i=0;i<n;i++)     {         if(a[i]>=avg)         {             c++;         }     }     printf("%d",c);
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,s=0;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    int avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
