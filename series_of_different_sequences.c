f61afebd2c3cfa72444121c1614fc3b65d3f6211
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2022-12-29 22:47:39 +0530

    //even==a*3-4; //odd==a*5+b; #include<stdio.h> int main() {     int n,i,a=11,b=22,c=100;     scanf("%d",&n);     printf("3 -2 5 2 ");     for(i=5;i<=n+1;i++)     {         if(i%2!=0)         {             printf("%d ",a);             a=(a*3)-4;         }         else         {            printf("%d ",b);            b=b+c;            c=c*5;         }     } }

---
+++ series_of_different_sequences.c
@@ -0,0 +1,23 @@
+//even==a*3-4;
+//odd==a*5+b;
+#include<stdio.h>
+int main()
+{
+    int n,i,a=11,b=22,c=100;
+    scanf("%d",&n);
+    printf("3 -2 5 2 ");
+    for(i=5;i<=n+1;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",a);
+            a=(a*3)-4;
+        }
+        else
+        {
+           printf("%d ",b);
+           b=b+c;
+           c=c*5;
+        }
+    }
+}
