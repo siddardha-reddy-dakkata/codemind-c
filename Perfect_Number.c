2c3bb6d0ba78e3700a033c8e1102d1d695662103
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2022-12-31 22:49:56 +0530

    #include<stdio.h> int main(){    int sum=0,a,i;    scanf("%d",&a);    for(i=1;i<a;i++)    {        if(a%i==0)        {            sum=sum + i;        }    }    if(sum==a)    {        printf("True");    }    else    {        printf("False");    }}

---
+++ Perfect_Number.c
@@ -0,0 +1,2 @@
+#include<stdio.h>
+int main(){    int sum=0,a,i;    scanf("%d",&a);    for(i=1;i<a;i++)    {        if(a%i==0)        {            sum=sum + i;        }    }    if(sum==a)    {        printf("True");    }    else    {        printf("False");    }}
