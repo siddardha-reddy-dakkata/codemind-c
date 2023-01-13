d679f7de5323840913ca038e34158b1b2f55eb82
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-13 21:37:29 +0530

    #include<stdio.h> int main(){     int a,sq1,sq2,r1,r2,ans1=0,ans2=0,i,j;     scanf("%d",&a);     int b=a;     sq1=a*a;     for(i=a;a!=0;a=a/10)     {         r1=a%10;         ans1=ans1*10 + r1;     }     sq2=ans1*ans1;     for(i=sq2;sq2!=0;sq2=sq2/10){         r2=sq2%10;         ans2=ans2*10+r2;     }     if(ans2==sq1) printf("True");     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
-int main(){int i,a,sq1,sq2,ans1=0,ans2=0,r1,r2;scanf("%d",&a);sq1=a*a;for(i=a;a!=0;a=a/10){r1=a%10;ans1=ans1*10 + r1;}sq2=ans1*ans1;for(i=sq2;sq2!=0;sq2=sq2/10){r2=sq2%10;ans2=ans2*10 + r2;}if(sq1==ans2) printf("True");else printf("False");}
+int main(){
+    int a,sq1,sq2,r1,r2,ans1=0,ans2=0,i,j;
+    scanf("%d",&a);
+    int b=a;
+    sq1=a*a;
+    for(i=a;a!=0;a=a/10)
+    {
+        r1=a%10;
+        ans1=ans1*10 + r1;
+    }
+    sq2=ans1*ans1;
+    for(i=sq2;sq2!=0;sq2=sq2/10){
+        r2=sq2%10;
+        ans2=ans2*10+r2;
+    }
+    if(ans2==sq1) printf("True");
+    else
+    {
+        printf("False");
+    }
+}
