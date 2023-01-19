69bc508340afdb79af6c81f4728985e05190c53d
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-19 20:55:31 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c,n,z,y,sa;     scanf("%d",&a);     b=a%10;     n=log10(a);     c=a/(pow(10,n));     y=c*pow(10,n);     z=b*pow(10,n);     sa=a-y+z-b+c;     printf("%d",sa); }

--- Shuffle_the_first_and_last_digits.c
+++ Shuffle_the_first_and_last_digits.c
@@ -2,13 +2,13 @@
 #include<math.h>
 int main()
 {
-    int a,b,c,n,z,y,ans;
+    int a,b,c,n,z,y,sa;
     scanf("%d",&a);
-    n=log10(a);
     b=a%10;
+    n=log10(a);
     c=a/(pow(10,n));
     y=c*pow(10,n);
     z=b*pow(10,n);
-    ans=a-y+z-b+c;
-    printf("%d",ans);
+    sa=a-y+z-b+c;
+    printf("%d",sa);
 }
