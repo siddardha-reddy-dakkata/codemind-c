364d9256dc8eac5df8eea5d49cc046615bfca5fc
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-25 09:40:54 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,j,m=n;     for(i=n+64;i>=65;i--)     {         for(j=m;j>=1;j--)         {             printf("%c ",i);         }         m--;         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int i,j,m=n;
+    for(i=n+64;i>=65;i--)
     {
-        for(j=1;j<=i;j++)
+        for(j=m;j>=1;j--)
         {
-            printf("%d",j);
+            printf("%c ",i);
         }
+        m--;
         printf("
 ");
     }
