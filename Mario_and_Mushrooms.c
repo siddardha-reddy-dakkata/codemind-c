99451eceadbc57a8e9d5d8592c18b07dae263044
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-27 18:34:04 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     a%=3;     if(a==0){         printf("NORMAL");     }     else if(a==1){         printf("HUGE");     }    else{         printf("SMALL");     } }

--- Mario_and_Mushrooms.c
+++ Mario_and_Mushrooms.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 int main()
-{ 
+{
     int a;
     scanf("%d",&a);
-    a=a%3;
-    if(a==0)
-    { 
+    a%=3;
+    if(a==0){
         printf("NORMAL");
     }
-    else if(a==1)
-    { 
+    else if(a==1){
         printf("HUGE");
     }
-    else{ printf("SMALL");
+   else{
+        printf("SMALL");
     }
-    
 }
