e1d0bc62a07c850531583056e4a9599f0a2fd1ff
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-29 07:33:06 +0530

    #include<stdio.h> int main() {     int count=0,a,i;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         count=count+i;     }     printf("%d",count); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,2 +1,11 @@
 #include<stdio.h>
-int main(){int count=0,a,i;scanf("%d",&a);for(i=1;i<=a;i++){count=count+i;}printf("%d",count);}
+int main()
+{
+    int count=0,a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        count=count+i;
+    }
+    printf("%d",count);
+}
