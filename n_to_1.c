ad33dd63ce479b1abcca94a7c24b8132454deb6f
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-01-29 07:34:13 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     for(i=a;i!=0;i--)     {         printf("%d ",i);     } }

--- n_to_1.c
+++ n_to_1.c
@@ -1,2 +1,10 @@
 #include<stdio.h>
-int main(){int a,i;scanf("%d",&a);for(i=a;i>=1;i--){printf("%d ",i);}}
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=a;i!=0;i--)
+    {
+        printf("%d ",i);
+    }
+}
